ffe0dce86cbef845c2e4b7b89fdb4fd3189093b1
TJ Stifter <tjstifter@gmail.com>
2023-06-13 16:46:18 -0400

    completes lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -17,15 +17,7 @@ function ShoppingList({ items }) {
 
   return (
     <div className="ShoppingList">
-      <Filter handleCategoryChange={handleCategoryChange}/>
-      {/* <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div> */}
+      <Filter onCategoryChange={handleCategoryChange}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
